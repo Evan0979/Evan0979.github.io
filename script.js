41e681ec4871a3adfe7722ef3bbf9637793166fb
Evan0979 <106424529+Evan0979@users.noreply.github.com>
2023-05-29 10:36:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function calculate() {
+    var twdAmount = document.getElementById("twdAmount").value;
+    
+    if (isNaN(twdAmount) || twdAmount <= 0) {
+      alert("請輸入有效數值！");
+      reset();
+      return;
+    }
+    
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      var rates = xhr.responseXML.getElementsByTagName("rate");
+      var resultDiv = document.getElementById("result");
+      resultDiv.innerHTML = "";
+        
+      for (var i = 0; i < rates.length; i++) {
+        var currency = rates[i].getElementsByTagName("currency")[0].textContent;
+        var rate = rates[i].getElementsByTagName("cashselling")[0].textContent;
+        var convertedAmount = (twdAmount / rate).toFixed(2);
+        var result = document.createElement("p");
+        result.textContent = "轉換成 " + currency + ": " + convertedAmount + " " + currency;
+        resultDiv.appendChild(result);
+      }
+    };
+    xhr.open("GET", "rate.xml", true);
+    xhr.send();
+  }
+  
+  function reset() {
+    document.getElementById("twdAmount").value = "";
+    document.getElementById("result").innerHTML = "";
+  }
+  
